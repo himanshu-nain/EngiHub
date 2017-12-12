29776ee0944623e9e5e2657305eec8722ce464de
Himanshu Nain <Himanshu_bt2k16@dtu.ac.in>
2017-12-12 18:04:04 +0530

    #deployment-bugs

--- EngiHub/settings.py
+++ EngiHub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5baa%0z9e0of-@698j-htxnl3f%1%si#cqxr8#6lz(ah^zkn9x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*'
